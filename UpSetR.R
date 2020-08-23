7d7816715aad2b95cc4011e5077c9878f6948ab6
Tian-Huang <69744327+Tian-Huang@users.noreply.github.com>
2020-08-23 02:39:59 -0400

    Add parameter for suffix

--- UpSetR.R
+++ UpSetR.R
@@ -9,16 +9,22 @@ parser$add_argument('-c','--col', nargs="+", help="The column index",required=TR
 parser$add_argument('-o','--output', help='Output directory plus prefix',required=TRUE)
 parser$add_argument('-n','--name', help='Name of each set',required=TRUE)
 
+parser$add_argument('-s','--suffix', help='Specify the number of characters in the input suffix and name the samples using trimmed file names (ex. 4 for .bam files)',required=FALSE,default=0)
 parser$add_argument('--nintersects', help='Number of intersections to plot',required=F,default=NA)
 
 args <- parser$parse_args()
 
 library("UpSetR")
 
-names<-args$name
 input<-args$input
 col<-args$col
 lt<-list()
+
+if(as.numeric(args$suffix)==0 && length(args$names)==length(input)){
+  names<-args$names
+}else{
+  names<-substr(input,max(gregexpr("/",input)[[1]])+1,nchar(input)-as.numeric(args$suffix))
+}
 
 for(i in length(names)){
   tb<-read.table(input[i],sep = "\t",header = F)
