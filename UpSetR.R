adf04c9a04ec342dea183f104030a01a45787a29
Tian-Huang <69744327+Tian-Huang@users.noreply.github.com>
2020-08-23 04:12:18 -0400

    fix bugs and add a few parameters

--- UpSetR.R
+++ UpSetR.R
@@ -8,30 +8,35 @@ parser$add_argument('-i','--input', nargs="+", help="Tab-deliminated tables",req
 parser$add_argument('-c','--col', help="The column index",required=TRUE)
 parser$add_argument('-o','--output', help='Output directory plus prefix',required=TRUE)
 
-parser$add_argument('-n','--name',nargs="+", help='Name of each set',required=F)
-parser$add_argument('-s','--suffix', help='Specify the number of characters in the input suffix and name the samples using trimmed file names (ex. 4 for .bam files)',required=FALSE,default=0)
-parser$add_argument('--nintersects', help='Number of intersections to plot',required=F,default=10)
+parser$add_argument('-n','--name',nargs="+", help='Name of each set',required=FALSE)
+parser$add_argument('-S','--suffix', help='Specify the number of characters in the input suffix and name the samples using trimmed file names (ex. 4 for .bam files)',required=FALSE,default=0)
+parser$add_argument('--nintersects', help='Number of intersections to plot',required=FALSE,default=10)
+parser$add_argument('-s','--size', nargs="+",
+                    help = "Specify figure size (width and height); default: 7 7",
+                    required=FALSE, default=c(7,7))
 
 args <- parser$parse_args()
 
 library("UpSetR")
 
 input<-args$input
-col<-args$col
+col<-as.integer(args$col)
+ni<-args$nintersects
+size<-as.numeric(args$size)
 lt<-list()
 
-if(as.numeric(args$suffix)==0 && length(args$names)==length(input)){
-  names<-args$names
+if(as.numeric(args$suffix)==0 && length(args$name)==length(input)){
+  names<-args$name
 }else{
   names<-substr(input,max(gregexpr("/",input)[[1]])+1,nchar(input)-as.numeric(args$suffix))
 }
 
-for(i in length(names)){
-  tb<-read.table(input[i],sep = "\t",header = F)
-  lt[names[i]]<-tb[,col]
+for(i in 1:length(names)){
+  tb<-read.table(input[i],sep = "\t",header = T)
+  lt[[names[i]]]<-tb[,col]
 }
 
-png(paste(args$output,".png",sep=""))
-upset(fromList(lt), nintersects = args$nintersects, mainbar.y.label = "log2(Counts)",
-         sets.x.label = "Counts", scale.intersections = "log2",mb.ratio=c(0.55,0.45), order.by = "freq")
+svg(file=paste(args$output,".svg",sep=""),width=size[1],height=size[2])
+upset(fromList(lt), sets=names, nintersects = ni, mainbar.y.label = "Counts",
+         sets.x.label = "Counts", mb.ratio=c(0.55,0.45), order.by = "freq")
 dev.off()
